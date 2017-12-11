bebe8c2119f2d71cdd6993dbfc8b27c18c217355
Unknown <rm.hobbel@telfort.nl>
2017-12-11 15:11:52 +0100

    Implemented Parcable

--- app/src/main/java/com/ags/projectseelion/POI.java
+++ app/src/main/java/com/ags/projectseelion/POI.java
@@ -1,12 +1,15 @@
 package com.ags.projectseelion;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Map;
 
 /**
  * Created by robin on 11-12-2017.
  */
 
-public class POI {
+public class POI implements Parcelable {
     private int number;
     private String name;
     private Map<String,String> description;
@@ -63,4 +66,43 @@ public class POI {
     public void setToVisit(boolean toVisit) {
         this.toVisit = toVisit;
     }
+
+    protected POI(Parcel in) {
+        number = in.readInt();
+        name = in.readString();
+        image = in.readInt();
+        longitude = in.readFloat();
+        latitude = in.readFloat();
+        catagory = (Catagory) in.readValue(Catagory.class.getClassLoader());
+        toVisit = in.readByte() != 0x00;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(number);
+        dest.writeString(name);
+        dest.writeInt(image);
+        dest.writeFloat(longitude);
+        dest.writeFloat(latitude);
+        dest.writeValue(catagory);
+        dest.writeByte((byte) (toVisit ? 0x01 : 0x00));
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<POI> CREATOR = new Parcelable.Creator<POI>() {
+        @Override
+        public POI createFromParcel(Parcel in) {
+            return new POI(in);
+        }
+
+        @Override
+        public POI[] newArray(int size) {
+            return new POI[size];
+        }
+    };
 }
