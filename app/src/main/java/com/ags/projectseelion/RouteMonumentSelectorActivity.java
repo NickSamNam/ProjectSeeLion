693a725dd726a1f3055b6f62bac11e9a0d5e6ae0
Unknown <kevinquist@zeelandnet.nl>
2017-12-11 14:12:20 +0100

    RouteMonumentSelector buttons work

--- app/src/main/java/com/ags/projectseelion/RouteMonumentSelectorActivity.java
+++ app/src/main/java/com/ags/projectseelion/RouteMonumentSelectorActivity.java
@@ -1,7 +1,10 @@
 package com.ags.projectseelion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RouteMonumentSelectorActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class RouteMonumentSelectorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_monument_selector);
+
+        Button btn_RouteSelector = findViewById(R.id.activity_route_monument_selector_btn_selectroute);
+        btn_RouteSelector.setOnClickListener(view -> startActivity(new Intent(this,RouteSelectorActivity.class)));
+        Button btn_MonumentSelector = findViewById(R.id.activity_route_monument_selector_btn_monuments);
+        btn_MonumentSelector.setOnClickListener(view -> startActivity(new Intent(this, MonumentSelectionActivity.class)));
     }
 }
