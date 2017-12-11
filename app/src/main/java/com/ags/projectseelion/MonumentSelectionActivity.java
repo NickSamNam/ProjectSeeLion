65b123e55a150f471f40d1142c9d57c845232ece
Sam Nicknam <s.nicknam@icloud.com>
2017-12-11 14:55:12 +0100

    Initial setup of MonumentSelectionActivity

--- app/src/main/java/com/ags/projectseelion/MonumentSelectionActivity.java
+++ app/src/main/java/com/ags/projectseelion/MonumentSelectionActivity.java
@@ -1,7 +1,9 @@
 package com.ags.projectseelion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MonumentSelectionActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MonumentSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monument_selection);
+
+        findViewById(R.id.activity_monument_selection_btn_continue).setOnClickListener(this::btnContinueOnClick);
+    }
+
+    private void btnContinueOnClick(View view) {
+        // TODO use correct intent and bundle
+        Intent intent = new Intent(getApplicationContext(), HistorischeActivity.class);
+        intent.putExtra("monuments", monuments);
+        startActivity(intent);
     }
 }
