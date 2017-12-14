c77aeee604faac74663558b2e9566cca4f96fbf5
Unknown <rm.hobbel@telfort.nl>
2017-12-14 13:20:11 +0100

    Split parser up into multiple methods

--- app/src/main/java/com/ags/projectseelion/JsonParser.java
+++ app/src/main/java/com/ags/projectseelion/JsonParser.java
@@ -8,7 +8,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,47 +19,22 @@ import java.util.Map;
 
 public class JsonParser {
     private Context context;
-    private JSONArray jsonArray = null;
-    private JSONObject json;
     public JsonParser(Context context){
         this.context = context;
     }
 
-    public POI parseToPOI(String location){
+    public JSONArray getJsonArray(String fileName){
+        JSONArray jsonArray = null;
         try {
-        InputStream ins = context.getResources().openRawResource(
-                context.getResources().getIdentifier(location,
-                        "raw", context.getPackageName()));
-
-        int size = 0;
-
-        size = ins.available();
-
-        byte[] buffer = new byte[size];
-        ins.read(buffer);
-        ins.close();
-        jsonArray = new JSONArray(new String(buffer, "UTF-8"));
-        json = jsonArray.getJSONObject(i);
-//        POI poi = new POI(
-//                json.getInt("Nummer"),   //nummer
-//                Location.convert(json.getString("Lat")), //latitude
-//                Location.convert(json.getString("Long")), //longitude
-//                json.getString("Naam"), //name
-//                json.getString("Opmerkingen"), // opmerkingen
-//                json.getString("Foto"),   //image
-//                json.getString("Tekst")   //description
-//        );
-        POI poi = new POI(
-                json.getInt("Nummer"),   //nummer
-                json.getString("Naam"),  //name
-                getDescription(json.getString("Tekst")),//description
-                json.getString("Foto"),   //imagename
-                
-        );
-
-
-
-        return poi;
+            InputStream ins = context.getResources().openRawResource(
+                    context.getResources().getIdentifier(fileName,
+                            "raw", context.getPackageName()));
+            int size = 0;
+            size = ins.available();
+            byte[] buffer = new byte[size];
+            ins.read(buffer);
+            ins.close();
+            jsonArray = new JSONArray(new String(buffer, "UTF-8"));
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -65,6 +42,46 @@ public class JsonParser {
             e.printStackTrace();
             return null;
         }
+        return jsonArray;
+    }
+
+    public List<POI> getAllPOIs(){
+        JSONArray array = getJsonArray("pois_historic_route");
+        ArrayList<POI> POIList = new ArrayList<>();
+        for (int i = 0; i < array.length();i++){
+            try {
+                POIList.add(parsePOI(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return POIList
+    }
+
+    private POI parsePOI(JSONObject json){
+        POI poi = null;
+        try {
+            poi = new POI(
+                    json.getInt("Nummer"),                  //nummer
+                    json.getString("Naam"),                 //name
+                    getDescription(json.getString("Tekst")),//description
+                    json.getString("Foto"),                 //imagename
+                    Location.convert(json.getString("OL")), //longitude
+                    Location.convert(json.getString("NB")), //latitude
+                    getCategory(json.getString("Naam"))     //Category
+            );
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return poi;
+    }
+
+    private Category getCategory(String naam) {
+        if(naam.isEmpty())
+            return Category.Intersection;
+        else
+            return Category.Building;
     }
 
     private Map<String,String> getDescription(String tekst) {
