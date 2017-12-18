7b006c1b1ef5b0ef6738d51d77237728a547bf67
Sam Nicknam <s.nicknam@icloud.com>
2017-12-18 13:27:32 +0100

    Fixed RecyclerView not working

--- app/src/main/java/com/ags/projectseelion/MonumentSelectionActivity.java
+++ app/src/main/java/com/ags/projectseelion/MonumentSelectionActivity.java
@@ -1,8 +1,9 @@
 package com.ags.projectseelion;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
@@ -19,7 +20,9 @@ public class MonumentSelectionActivity extends AppCompatActivity {
         monuments = null;
 
         findViewById(R.id.activity_monument_selection_btn_continue).setOnClickListener(this::btnContinueOnClick);
-        ((RecyclerView) findViewById(R.id.activity_monument_selection_rv_monuments)).setAdapter(new MonumentAdapter(monuments));
+        RecyclerView rvShifts = findViewById(R.id.activity_monument_selection_rv_monuments);
+        rvShifts.setLayoutManager(new LinearLayoutManager(this));
+        rvShifts.setAdapter(new MonumentAdapter(monuments));
     }
 
     private void btnContinueOnClick(View view) {
