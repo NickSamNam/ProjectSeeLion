31de37eab2cef7063aa005e6e47f2640bd64b058
Sam Nicknam <s.nicknam@icloud.com>
2017-12-20 20:35:41 +0100

    Removed more unused code

--- app/src/main/java/com/ags/projectseelion/MapActivity.java
+++ app/src/main/java/com/ags/projectseelion/MapActivity.java
@@ -588,50 +588,5 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
         }
     }
-
-    /**
-     * A method to download json data from url
-     */
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
-
-            // Creating an http connection to communicate with url
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            // Connecting to url
-            urlConnection.connect();
-
-            // Reading data from url
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuilder sb = new StringBuilder();
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            data = sb.toString();
-            Log.d("downloadUrl", data);
-            br.close();
-
-        } catch (Exception e) {
-            Log.d("Exception", e.toString());
-        } finally {
-            if (iStream != null) {
-                iStream.close();
-            }
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
-        return data;
-    }
 }
 
